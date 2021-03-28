0305ea54ac62cc1a7c66e786fa59e1d0f9759874
Christoph-Szabo <cszabo@casteq.com>
2021-03-28 23:31:16 +0200

    Initial commit

---
+++ app/src/main/java/com/szabo/kotlinyoutubelbta/CourseLessonActivity.kt
@@ -0,0 +1,24 @@
+package com.szabo.kotlinyoutubelbta
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_course_lesson.*
+
+class CourseLessonActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_course_lesson)
+
+        val courseLink = intent.getStringExtra(CourseDetailActivity.CourseLessonViewHolder.COURSE_LESSON_LINK_KEY)
+
+        webview_course_lesson.settings.javaScriptEnabled = true
+        webview_course_lesson.settings.loadWithOverviewMode = true
+        webview_course_lesson.settings.useWideViewPort = true
+
+        if (courseLink != null) {
+            webview_course_lesson.loadUrl(courseLink)
+        }
+    }
+}
